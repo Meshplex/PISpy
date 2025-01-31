29da30f756e59c13435c6526a47199a2b376b420
Meshplex <tercan.piricek1@gmail.com>
2025-01-31 01:32:37 +0100

    piezo

--- src/PiSpyBackend/PiSpyBackend.Test/UnitTests/PiezoSensorTests.cs
+++ src/PiSpyBackend/PiSpyBackend.Test/UnitTests/PiezoSensorTests.cs
@@ -1,46 +1,21 @@
 using System;
-using System.Device.Gpio;
+using System.Device.Pwm;
+using System.Threading;
 using Xunit;
 
 namespace SensorTests
 {
-    public class PiezoSensorTests : IDisposable
+    public class PiezoSensorTests
     {
-        private readonly GpioController _controller;
-        private const int BuzzerPin = 18; // BCM 18
-
-        public PiezoSensorTests()
-        {
-            // Erzeugt einen GPIO-Controller
-            _controller = new GpioController();
-            // Buzzer-Pin als Ausgang setzen
-            _controller.OpenPin(BuzzerPin, PinMode.Output);
-        }
-
         [Fact]
-        public void BuzzerShouldBeep()
+        public void Buzzer_Should_Beep_Shortly()
         {
-            // Schaltet den Buzzer ein (High)
-            _controller.Write(BuzzerPin, PinValue.High);
+            int buzzerPin = 18;
 
-            // Lässt ihn 2 Sekunden piepen
-            Console.WriteLine("Buzzer an (aktiver Piezo). Jetzt sollte es piepen!");
-            System.Threading.Thread.Sleep(500);
-
-            // Schaltet wieder aus (Low)
-            _controller.Write(BuzzerPin, PinValue.Low);
-            Console.WriteLine("Buzzer aus. Piepen sollte gestoppt sein.");
-
-            // Für den Test kein echter 'Assert'; du könntest z. B. nur kontrollieren,
-            // dass dein Code ohne Fehler durchläuft.
-            Assert.True(true);
-        }
-
-        public void Dispose()
-        {
-            // Pin aufräumen
-            _controller.ClosePin(BuzzerPin);
-            _controller.Dispose();
+            using (var buzzer = new Buzzer(buzzerPin))
+            {
+                buzzer.Beep(frequency: 1000, duration: 500);
+            }
         }
     }
 }
