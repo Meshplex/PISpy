ac69cecf1c0427033096c183b6a5249059ce46df
Meshplex <tercan.piricek1@gmail.com>
2025-02-01 23:50:58 +0100

    rfid bug fix

--- src/PiSpyBackend/PiSpyBackend.Application/Services/RfidService.cs
+++ src/PiSpyBackend/PiSpyBackend.Application/Services/RfidService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Device.Gpio;
 using System.Device.Spi;
+using System.Numerics;
 using Iot.Device.Mfrc522;
 using Iot.Device.Rfid;
 using PiSpyBackend.Domain.Interfaces;
@@ -12,14 +13,14 @@ namespace PiSpyBackend.Application
         private const int SpiBusId = 0;
         private const int ChipSelectLine = 0; // CE0 (Pin 24)
         private const int ResetPin = 25;      // GPIO25 (Pin 22)
-        
+
         public object Run()
         {
             try
             {
                 // Erstelle den GPIO-Controller in einem using-Block, damit er korrekt entsorgt wird.
                 using var gpio = new GpioController(PinNumberingScheme.Logical);
-                
+
                 var spiSettings = new SpiConnectionSettings(SpiBusId, ChipSelectLine)
                 {
                     ClockFrequency = 10_000_000,
@@ -28,7 +29,7 @@ namespace PiSpyBackend.Application
 
                 // Erstelle das SPI-Gerät.
                 using var spi = SpiDevice.Create(spiSettings);
-                
+
                 // Initialisiere den RFID-Leser.
                 using var reader = new MfRc522(spi, ResetPin, gpio, false);
 
@@ -41,11 +42,17 @@ namespace PiSpyBackend.Application
                 var atqa = new byte[2];
                 if (reader.IsCardPresent(atqa, false))
                 {
-                    // Versuche, die UID der Karte auszulesen.
                     if (reader.ListenToCardIso14443TypeA(out Data106kbpsTypeA cardData, TimeSpan.FromSeconds(2)))
                     {
-                        // UID in einen String umwandeln (z. B. "04A3B2C1")
-                        return BitConverter.ToString(cardData.NfcId).Replace("-", "");
+                        // UID-Bytes auslesen (z. B. [0xC3, 0x84, 0x34, 0x02, 0xB1])
+                        byte[] uidBytes = cardData.NfcId;
+
+                        // Bytes in Big-Endian-Reihenfolge umwandeln (falls nötig)
+                        Array.Reverse(uidBytes); // Nur wenn die Bytes in Little-Endian gelesen werden
+
+                        // Bytes in eine Dezimalzahl konvertieren
+                        BigInteger uidDecimal = new BigInteger(uidBytes, isUnsigned: true, isBigEndian: true);
+                        return uidDecimal.ToString();
                     }
                 }
             }
