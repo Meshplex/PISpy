35722b3cd823f3809fe06738f58f52c69de3e06e
Meshplex <tercan.piricek1@gmail.com>
2025-02-02 22:28:50 +0100

    led

--- src/PiSpyBackend/PiSpyBackend.Test/UnitTests/NeoPixelLedRingTest.cs
+++ src/PiSpyBackend/PiSpyBackend.Test/UnitTests/NeoPixelLedRingTest.cs
@@ -1,71 +1,33 @@
-using System;
+
+using rpi_ws281x;
 using System.Device.Spi;
 using System.Drawing;
-using Iot.Device.Ws28xx; // Namespace aus dem Iot.Device.Bindings-Paket
-using Xunit;
 
 namespace PiSpyBackend.Test.UnitTests
 {
     public class NeoPixelLedRingTests
     {
-        private readonly Ws2812b _ledRing;
-        private const int LedCount = 24;
-
-        public NeoPixelLedRingTests()
-        {
-            var spiSettings = new SpiConnectionSettings(0, 0)
-            {
-                ClockFrequency = 2400000,
-                Mode = SpiMode.Mode0,
-                DataBitLength = 8,
-            };
-
-            var spiDevice = SpiDevice.Create(spiSettings);
-            _ledRing = new Ws2812b(spiDevice, LedCount);
-        }
 
         [Fact]
         public void SetAllLedsToRedTest()
         {
-            for (int i = 0; i < LedCount; i++)
-            {
-                _ledRing.Image.SetPixel(i, 0, Color.Red);
-            }
-            _ledRing.Update();
+            var settings = Settings.CreateDefaultSettings();
+        
+            // Kanal für die LEDs konfigurieren
+            settings.Channels[0] = new Channel(
+                ledCount: 24,       // Anzahl der LEDs
+                gpioPin: 21,        // GPIO 21
+                brightness: 255,    // Helligkeit (0-255)
+                invert: false,      // Signal nicht invertieren
+                stripType: StripType.WS2812_STRIP // Typ der LEDs
+            );
 
-            // Warte etwas, damit du den Farbwechsel am Ring beobachten kannst.
-            Thread.Sleep(2000);
+        // LED-Controller erstellen
+            var controller = new WS281x(settings);
 
-            // Erreichen wir diesen Punkt ohne Exception, gilt der Test als bestanden.
-            Assert.True(true);
-        }
-
-        [Fact]
-        public void CycleColorsTest()
-        {
-            // Eine Auswahl an Farben zum Durchlaufen.
-            Color[] colors = new Color[] 
-            { 
-                Color.Red, 
-                Color.Green, 
-                Color.Blue, 
-                Color.Yellow, 
-                Color.Purple, 
-                Color.Cyan, 
-                Color.White 
-            };
-
-            foreach (Color color in colors)
-            {
-                for (int i = 0; i < LedCount; i++)
-                {
-                    _ledRing.Image.SetPixel(i, 0, color);
-                }
-                _ledRing.Update();
-                Thread.Sleep(500);
-            }
-
-            Assert.True(true);
+        // Beispiel: Erste LED auf Rot setzen
+            controller.SetLEDColor(0, 3, Color.Red);
+            controller.Render(); // Änderungen anzeigen
         }
     }
 }
