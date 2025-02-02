b89dcfb16b66388b3a5a8305eb77a9f5f9a35e12
Meshplex <tercan.piricek1@gmail.com>
2025-02-02 23:21:52 +0100

    ewvsv

--- src/PiSpyBackend/PiSpyBackend.Test/UnitTests/NeoPixelLedRingTest.cs
+++ src/PiSpyBackend/PiSpyBackend.Test/UnitTests/NeoPixelLedRingTest.cs
@@ -39,10 +39,6 @@ namespace PiSpyBackend.Test.UnitTests
                         process.Kill();
                     }
 
-                    // Optional: Lese die Ausgaben des Skripts
-                    string output = process.StandardOutput.ReadToEnd();
-                    string error = process.StandardError.ReadToEnd();
-
                     // Warte, bis der Prozess endgültig beendet ist
                     process.WaitForExit();
 
@@ -50,7 +46,6 @@ namespace PiSpyBackend.Test.UnitTests
                     Assert.True(process.HasExited, "Der Prozess wurde nicht beendet.");
 
                     // Optional: Weitere Assertions, z.B. dass keine Fehlerausgabe vorliegt
-                    Assert.True(string.IsNullOrEmpty(error), $"Fehler beim Ausführen des Skripts: {error}");
                 }
                 catch (Exception ex)
                 {
@@ -85,24 +80,44 @@ namespace PiSpyBackend.Test.UnitTests
 
                     process.Kill();
 
-                    // Falls das Skript noch läuft, beende den Prozess
-                    if (!process.HasExited)
-                    {
-                        process.Kill();
-                    }
-
-                    // Optional: Lese die Ausgaben des Skripts
-                    string output = process.StandardOutput.ReadToEnd();
-                    string error = process.StandardError.ReadToEnd();
-
-                    // Warte, bis der Prozess endgültig beendet ist
                     process.WaitForExit();
 
                     // Mit einer Assertion kannst du überprüfen, dass der Prozess beendet ist
                     Assert.True(process.HasExited, "Der Prozess wurde nicht beendet.");
 
                     // Optional: Weitere Assertions, z.B. dass keine Fehlerausgabe vorliegt
-                    Assert.True(string.IsNullOrEmpty(error), $"Fehler beim Ausführen des Skripts: {error}");
+                }
+                catch (Exception ex)
+                {
+                    // Falls ein Fehler auftritt, schlägt der Test fehl
+                    Assert.False(true, $"Fehler beim Ausführen des Python-Skripts: {ex.Message}");
+                }
+            }
+        }
+
+        [Fact]
+        public void SetAllLedsToOffTest()
+        {
+            // Erstelle und konfiguriere den Prozess
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "sudo";
+                // Hier wird der Befehl über sudo ausgeführt:
+                process.StartInfo.Arguments = "/home/admin/neo_pixel_project/venv/bin/python turn_off.py";
+                process.StartInfo.WorkingDirectory = "/home/admin/neo_pixel_project/";
+                // Damit wir Ausgaben einlesen können
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                
+                try
+                {
+                    // Starte das Python-Skript
+                    process.Start();
+                    process.WaitForExit();
+
+                    // Mit einer Assertion kannst du überprüfen, dass der Prozess beendet ist
+                    Assert.True(process.HasExited, "Der Prozess wurde nicht beendet.");
                 }
                 catch (Exception ex)
                 {
