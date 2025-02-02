386d8f5674b7bbb69524741a27af7187b74dc018
Meshplex <tercan.piricek1@gmail.com>
2025-02-02 22:03:56 +0100

    gg

---
+++ src/PiSpyBackend/PiSpyBackend.Test/UnitTests/NeoPixelLedRingTest.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Device.Spi;
+using System.Drawing;
+using Iot.Device.Ws28xx; // Namespace aus dem Iot.Device.Bindings-Paket
+using Xunit;
+
+namespace NeoPixelLedRingTest
+{
+    public class NeoPixelLedRingTests
+    {
+        private readonly Ws2812b _ledRing;
+        private const int LedCount = 24;
+
+        public NeoPixelLedRingTests()
+        {
+            var spiSettings = new SpiConnectionSettings(0, 0)
+            {
+                ClockFrequency = 2400000,
+                Mode = SpiMode.Mode0,
+                DataBitLength = 8,
+            };
+
+            var spiDevice = SpiDevice.Create(spiSettings);
+            _ledRing = new Ws2812b(spiDevice, LedCount);
+        }
+
+        [Fact]
+        public void SetAllLedsToRed_Test()
+        {
+            for (int i = 0; i < LedCount; i++)
+            {
+                _ledRing.Image.SetPixel(i, 0, Color.Red);
+            }
+            _ledRing.Update();
+
+            // Warte etwas, damit du den Farbwechsel am Ring beobachten kannst.
+            Thread.Sleep(2000);
+
+            // Erreichen wir diesen Punkt ohne Exception, gilt der Test als bestanden.
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void CycleColors_Test()
+        {
+            // Eine Auswahl an Farben zum Durchlaufen.
+            Color[] colors = new Color[] 
+            { 
+                Color.Red, 
+                Color.Green, 
+                Color.Blue, 
+                Color.Yellow, 
+                Color.Purple, 
+                Color.Cyan, 
+                Color.White 
+            };
+
+            foreach (Color color in colors)
+            {
+                for (int i = 0; i < LedCount; i++)
+                {
+                    _ledRing.Image.SetPixel(i, 0, color);
+                }
+                _ledRing.Update();
+                Thread.Sleep(500);
+            }
+
+            Assert.True(true);
+        }
+    }
+}
